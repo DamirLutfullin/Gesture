b7aba43d29e7106919a592995fad01dd1a1d0d99
Damir <dmr.lutfullin@gmail.com>
2021-06-10 13:45:30 +0500

    complete

---
+++ Gesture/TickleGestureRecognizer.swift
@@ -0,0 +1,79 @@
+//
+//  TickleGestureRecognizer.swift
+//  Gesture
+//
+//  Created by Damir L on 10.06.2021.
+//
+
+import UIKit
+
+
+class ​TickleGestureRecognizer: UIGestureRecognizer {
+    
+    private let requiredTickles = 2
+    private let distanceForTickletGesture: CGFloat = 25
+    
+    enum TickletDirection {
+        case right, left, unknow
+    }
+    
+    private var tickleCount = 0
+    private var tickleStartLocation: CGPoint = .zero
+    private var latestDirection: TickletDirection = .unknow
+    
+    override func reset() {
+        tickleCount = 0
+        latestDirection = .unknow
+        tickleStartLocation = .zero
+        
+        if state == .possible {
+            state = .failed
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        guard let touch = touches.first else { return }
+        tickleStartLocation = touch.location(in: view)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        guard let touch = touches.first else { return }
+        
+        let tickleLocation = touch.location(in: view)
+        let horiaontalDifference = tickleLocation.x - tickleStartLocation.x
+        
+        if abs(horiaontalDifference) < distanceForTickletGesture {
+            return
+        }
+        
+        let direction: TickletDirection
+        
+        if horiaontalDifference < 0 {
+            direction = .left
+        } else {
+            direction = .right
+        }
+        
+        if latestDirection == .unknow || (latestDirection == .left
+                                            && direction == .right) ||
+        (latestDirection == .right
+            && direction == .left) {
+            tickleStartLocation = tickleLocation
+            latestDirection = direction
+            tickleCount += 1
+        }
+        
+        if state == .possible && tickleCount > requiredTickles {
+            state = .ended
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        reset()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        reset()
+    }
+
+}
--- Gesture/ViewController.swift
+++ Gesture/ViewController.swift
@@ -6,14 +6,121 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var bananaPan: UIPanGestureRecognizer!
+    @IBOutlet var monkeyPan: UIPanGestureRecognizer!
+    
+    private var chompPlayer: AVAudioPlayer?
+    private var laughPlayer: AVAudioPlayer?
+    
+    func createPlayer(from filename: String) -> AVAudioPlayer? {
+      guard let url = Bundle.main.url(
+        forResource: filename,
+        withExtension: "caf"
+        ) else {
+          return nil
+      }
+      var player = AVAudioPlayer()
 
+      do {
+        try player = AVAudioPlayer(contentsOf: url)
+        player.prepareToPlay()
+      } catch {
+        print("Error loading \(url.absoluteString): \(error)")
+      }
+
+      return player
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let imageViews = view.subviews.filter {
+            $0 is UIImageView
+        }
+        for imageView in imageViews {
+            let tapGesture = UITapGestureRecognizer(
+                target: self,
+                action: #selector(handleTap)
+            )
+            
+            tapGesture.delegate = self
+            imageView.addGestureRecognizer(tapGesture)
+            
+            tapGesture.require(toFail: monkeyPan)
+            tapGesture.require(toFail: bananaPan)
+            
+            let tickleGesture = ​TickleGestureRecognizer(target: self, action: #selector(handleTickle))
+            tickleGesture.delegate = self
+            imageView.addGestureRecognizer(tickleGesture)
+          
+        }
+        
+        chompPlayer = createPlayer(from: "chomp")
+        laughPlayer = createPlayer(from: "laugh")
     }
+    
+    
+    
+    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
+        
+        let translation = gesture.translation(in: view)
+        
+        guard let gestureView = gesture.view else {
+            return
+        }
+        
+        gestureView.center = CGPoint(
+            x: gestureView.center.x + translation.x,
+            y: gestureView.center.y + translation.y
+        )
+        
+        gesture.setTranslation(.zero, in: view)
+    }
+    
+    
+    @IBAction func handlePinch(_ gesture: UIPinchGestureRecognizer) {
+        guard let gestureView = gesture.view else {
+            return
+        }
+        
+        gestureView.transform = gestureView.transform.scaledBy(
+            x: gesture.scale,
+            y: gesture.scale
+        )
+        gesture.scale = 1
 
+    }
+    
+    @IBAction func handleRotate(_ gesture: UIRotationGestureRecognizer) {
+        guard let gestureView = gesture.view else {
+          return
+        }
+        gestureView.transform = gestureView.transform.rotated(
+          by: gesture.rotation
+        )
+        gesture.rotation = 0
+    }
+    
+    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+        chompPlayer?.play()
+    }
+    
+    @objc func handleTickle(_ gesture: UITapGestureRecognizer) {
+        laughPlayer?.play()
+    }
+    
+}
 
+extension ViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
+    }
+    
 }
 
